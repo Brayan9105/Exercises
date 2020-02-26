809632d275385d5b8b95728ba174cdcf7dba96e6
Brayan Lopez <brayan.lopez@koombea.com>
2020-02-26 12:07:55 -0500

    Add tests to product crud

--- CRUD/product.rb
+++ CRUD/product.rb
@@ -1,3 +1,5 @@
+require "minitest/autorun"
+
 class Product
     def initialize
         @@products = []
@@ -47,7 +49,7 @@ class Product
                 product[:brand] = fields[:name] if fields[:name]
                 product[:description] = fields[:name] if fields[:name]
                 product[:quantity] = fields[:name] if fields[:name]
-                break
+                return product
             end
         end
 
@@ -78,19 +80,95 @@ class Product
           user_valid &&= value == user[field]
         end
         user_valid
-      end
     end
+    
 
     
 end
 
-p = Product.new
-p p.all
-p.create(name: "Sugar", value: 200, brand: "Manuelita", description: "Some description", quantity: 5)
-p p.all
-# p.update(1, {name: "sazz", last: "otro"})
 
-# p.destroy(id: 1)
-#  p p.all
-# describe "" do
-# end
+
+describe Product do
+
+    before do
+        @p = Product.new
+    end
+
+    describe "When create a product" do
+        it "Must respond false" do
+            _(@p.create(name: "Sugar", brand: "Manuelita", description: "Some description", quantity: 5)).must_be_nil
+        end
+
+        it "Must respond with an array" do
+            _(@p.create(name: "Sugar", value: 200, brand: "Manuelita", description: "Some description", quantity: 5)).must_equal [{:id=>1, :name=>"Sugar", :value=>200, :brand=>"Manuelita", :description=>"Some description", :quantity=>5}]
+        end
+    end
+
+    describe "When query the number of products" do 
+        it "Must respond 1" do
+            @p.create(name: "Sugar", value: 200, brand: "Manuelita", description: "Some description", quantity: 5)
+            _(@p.count).must_equal 1
+        end
+
+        it "Must respond 2" do
+            @p.create(name: "Sugar", value: 200, brand: "Manuelita", description: "Some description", quantity: 5)
+            @p.create(name: "Butter", value: 300, brand: "Margarina", description: "Some description", quantity: 8)
+            _(@p.count).must_equal 2
+        end
+    end
+
+    describe "When query all the products" do
+        it "Must respond a with a empty array" do
+            _(@p.all).must_equal []
+        end
+
+        it "Must respond with an array of all products" do
+            @p.create(name: "Sugar", value: 200, brand: "Manuelita", description: "Some description", quantity: 5)
+            @p.create(name: "Butter", value: 300, brand: "Margarina", description: "Some description", quantity: 8)
+            _(@p.all).must_equal [{:id=>1, :name=>"Sugar", :value=>200, :brand=>"Manuelita", :description=>"Some description", :quantity=>5}, {:id=>2, :name=>"Butter", :value=>300, :brand=>"Margarina", :description=>"Some description", :quantity=>8}]
+        end
+    end
+
+    # describe "When query for a specific product" do
+    #     it "Must respond ···" do
+    #         @p.create(name: "Sugar", value: 200, brand: "Manuelita", description: "Some description", quantity: 5)
+    #         _(@p.find(id: 2)).must be {}
+    #     end
+    # end
+
+    describe "When query products with a criteria" do
+        it "Must respond with a empty list" do
+            @p.create(name: "Sugar", value: 200, brand: "Manuelita", description: "Some description", quantity: 5)
+            _(@p.where(value: 300)).must_equal []
+        end
+
+        it "Must respond with a list of product with the criteria" do
+            @p.create(name: "Sugar", value: 200, brand: "Manuelita", description: "Some description", quantity: 5)
+            @p.create(name: "Butter", value: 200, brand: "Margarina", description: "Some description", quantity: 8)
+            _(@p.where(value: 200)).must_equal [{:id=>1, :name=>"Sugar", :value=>200, :brand=>"Manuelita", :description=>"Some description", :quantity=>5}, {:id=>2, :name=>"Butter", :value=>200, :brand=>"Margarina", :description=>"Some description", :quantity=>8}]
+        end
+    end
+
+    describe "When update a product" do
+        # it "Must respond nil" do
+        #     @p.create(name: "Sugar", value: 200, brand: "Manuelita", description: "Some description", quantity: 5)
+        #     @p.create(name: "Butter", value: 200, brand: "Margarina", description: "Some description", quantity: 8)
+        #     _(@p.update(1, {name: "New product", value: 500})).must_equal {:id=>1, :name=>"New product", :value=>"New product", :brand=>"New product", :description=>"New product", :quantity=>"New product"}
+        # end
+
+        # it "Must respond nil" do
+        #     @p.create(name: "Sugar", value: 200, brand: "Manuelita", description: "Some description", quantity: 5)
+        #     @p.create(name: "Butter", value: 200, brand: "Margarina", description: "Some description", quantity: 8)
+        #     _(@p.update(1, {name: "New product", value: 500, brand: "Some brand"})).must_equal {:id=>1, :name=>"New product", :value=>"New product", :brand=>"New product", :description=>"New product", :quantity=>"New product"}
+        # end
+    end
+
+    describe "When delete a product" do
+        it "Must respond " do
+            @p.create(name: "Sugar", value: 200, brand: "Manuelita", description: "Some description", quantity: 5)
+            @p.create(name: "Butter", value: 200, brand: "Margarina", description: "Some description", quantity: 8)
+            _(@p.destroy(id: 1)).must_be_nil
+        end
+    end
+
+end
